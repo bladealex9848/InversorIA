f4c196b70edc7e8a67ea436a3f562d8552e5b479
Alexander Oviedo Fadul <bladealex@gmail.com>
2025-02-16 12:58:13 -0500

    Commit inicial

---
+++ README.md
@@ -0,0 +1,50 @@
+![Logo de InversorIA](https://github.com/bladealex9848/InversorIA/blob/main/assets/logo.jpg)
+
+# InversorIA 📈
+
+## Descripción
+InversorIA es un agente de IA especializado en inversión bursátil. Te ayuda a comprender el análisis técnico, las estrategias de inversión y la gestión de riesgo, basándose en los libros de expertos en el campo.
+
+## Funcionalidades
+* Identifica tendencias del mercado.
+* Reconoce patrones de velas y niveles técnicos.
+* Analiza el volumen de transacciones.
+* Explica estrategias de inversión.
+* Asesora sobre la gestión de riesgo.
+* Brinda información sobre opciones financieras.
+
+## Limitaciones
+* No puede acceder a información externa ni a detalles específicos del proyecto que no estén en los documentos.
+* Su conocimiento se limita a la información disponible en los documentos.
+
+## Instalación
+
+1. Asegúrate de tener Python 3.8 o superior instalado en tu máquina.
+2. Clona este repositorio: `git clone https://github.com/bladealex9848/InversorIA.git`
+3. Navega al directorio del proyecto: `cd InversorIA`
+4. Instala las dependencias: `pip install -r requirements.txt`
+5. Crea un archivo `.streamlit/secrets.toml` y añade tu ASSISTANT_ID de OpenAI:
+   ```
+   ASSISTANT_ID = "tu-assistant-id-aqui"
+   ```
+6. Configura tu clave API de OpenAI como variable de entorno o en el archivo `secrets.toml`.
+
+## Uso
+
+1. Ejecuta la aplicación: `streamlit run app.py`
+2. Abre tu navegador y ve a `http://localhost:8501`
+3. Comienza a interactuar con el Asistente Virtual escribiendo tus preguntas o solicitudes.
+
+## Contribuciones
+
+Las contribuciones son bienvenidas. Por favor, haz un fork del repositorio, crea una nueva rama para tus cambios, y envía un pull request.
+
+## Licencia
+
+Este proyecto está bajo la licencia MIT. Consulta el archivo `LICENSE` para más detalles.
+
+## Autor
+
+Creado por Alexander Oviedo Fadul
+
+[GitHub](https://github.com/bladealex9848) | [Website](https://alexanderoviedofadul.dev) | [LinkedIn](https://www.linkedin.com/in/alexander-oviedo-fadul/) | [Instagram](https://www.instagram.com/alexander.oviedo.fadul) | [Twitter](https://twitter.com/alexanderofadul) | [Facebook](https://www.facebook.com/alexanderof/) | [WhatsApp](https://api.whatsapp.com/send?phone=573015930519&text=Hola%20!Quiero%20conversar%20contigo!%20)
---
+++ app.py
@@ -0,0 +1,161 @@
+# Importación de bibliotecas necesarias
+import os
+import openai
+import streamlit as st
+import time
+
+import streamlit as st
+import openai
+
+# Configuración de la página
+st.set_page_config(
+    page_title="InversorIA",
+    page_icon="📈",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': 'https://www.alexanderoviedofadul.dev/',
+        'Report a bug': None,
+        'About': "InversorIA: Tu agente de IA para la inversión bursátil. Aprende sobre análisis técnico, estrategias de inversión y gestión de riesgo."
+    }
+)
+
+# Función para verificar si el archivo secrets.toml existe
+def secrets_file_exists():
+    secrets_path = os.path.join('.streamlit', 'secrets.toml')
+    return os.path.isfile(secrets_path)
+
+# Intentar obtener el ID del asistente de OpenAI desde st.secrets si el archivo secrets.toml existe
+if secrets_file_exists():
+    try:
+        ASSISTANT_ID = st.secrets['ASSISTANT_ID']
+    except KeyError:
+        ASSISTANT_ID = None
+else:
+    ASSISTANT_ID = None
+
+# Si no está disponible, pedir al usuario que lo introduzca
+if not ASSISTANT_ID:
+    ASSISTANT_ID = st.sidebar.text_input('Introduce el ID del asistente de OpenAI', type='password')
+
+# Si aún no se proporciona el ID, mostrar un error y detener la ejecución
+if not ASSISTANT_ID:
+    st.sidebar.error("Por favor, proporciona el ID del asistente de OpenAI.")
+    st.stop()
+
+assistant_id = ASSISTANT_ID
+
+# Inicialización del cliente de OpenAI
+client = openai
+
+# Título de la aplicación
+st.title("Bienvenido a InversorIA 📈")
+
+# Contador de visitantes
+st.write("""
+        ![Visitantes](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%inversoria.streamlit.app&label=Visitantes&labelColor=%235d5d5d&countColor=%231e7ebf&style=flat)
+        """)
+
+# Descripción de la aplicación
+st.markdown("""
+### 📈 ¡Hola! Soy InversorIA, tu agente de IA para la inversión bursátil
+
+Estoy aquí para ayudarte a comprender el análisis técnico, las estrategias de inversión y la gestión de riesgo, basándome en los libros de la serie "Crea tu Propia Riqueza" de Alejandro Cardona.
+
+#### ¿Qué puedo hacer por ti hoy? 🤔
+
+* Identificar tendencias alcistas y bajistas en el mercado.
+* Reconocer patrones de velas japonesas como martillos, 'hangers' y 'gaps'.
+* Identificar soportes y resistencias utilizando promedios móviles.
+* Analizar el volumen de transacciones para confirmar señales.
+* Explicar estrategias de inversión como 'Piso Fuerte', 'Primer Gap al Alza', 'Gap Normal al Alza' y 'Gap Bajista al Alza'.
+* Asesorar sobre la gestión de riesgo y el uso del stop loss.
+* Brindar información sobre el uso de opciones financieras (calls y puts) para el apalancamiento.
+
+**¡No dudes en consultarme cualquier inquietud sobre el análisis técnico o las estrategias de inversión!**
+
+*Recuerda: Mi conocimiento se basa en los libros de expertos en mercados de acciones y puede no ser aplicable a otros mercados o estrategias de inversión. No tengo acceso a información en tiempo real ni puedo realizar análisis de datos en vivo.*
+""")
+
+# Inicialización de variables de estado de sesión
+st.session_state.start_chat = True
+if "thread_id" not in st.session_state:
+    st.session_state.thread_id = None
+
+# Cargar la clave API de OpenAI
+API_KEY = os.environ.get('OPENAI_API_KEY') or st.secrets.get('OPENAI_API_KEY')
+if not API_KEY:
+    API_KEY = st.sidebar.text_input('Introduce tu clave API de OpenAI', type='password')
+
+if not API_KEY:
+    st.sidebar.error("Por favor, proporciona una clave API para continuar.")
+    st.stop()
+
+openai.api_key = API_KEY
+
+def process_message_with_citations(message):
+    """Extraiga y devuelva solo el texto del mensaje del asistente."""
+    if hasattr(message, 'content') and len(message.content) > 0:
+        message_content = message.content[0]
+        if hasattr(message_content, 'text'):
+            nested_text = message_content.text
+            if hasattr(nested_text, 'value'):
+                return nested_text.value
+    return 'No se pudo procesar el mensaje'
+
+# Crear un hilo de chat inmediatamente después de cargar la clave API
+if not st.session_state.thread_id:
+    thread = client.beta.threads.create()
+    st.session_state.thread_id = thread.id
+    st.write("ID del hilo: ", thread.id)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("¿Cómo puedo ayudarte hoy?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("usuario"):
+        st.markdown(prompt)
+
+    # Enviar mensaje del usuario
+    client.beta.threads.messages.create(
+        thread_id=st.session_state.thread_id,
+        role="user",
+        content=prompt
+    )
+
+    # Crear una ejecución para el hilo de chat
+    run = client.beta.threads.runs.create(
+        thread_id=st.session_state.thread_id,
+        assistant_id=assistant_id
+    )
+
+    while run.status != 'completed':
+        time.sleep(1)
+        run = client.beta.threads.runs.retrieve(
+            thread_id=st.session_state.thread_id,
+            run_id=run.id
+        )
+
+    # Recuperar mensajes agregados por el asistente
+    messages = client.beta.threads.messages.list(
+    thread_id=st.session_state.thread_id
+    )
+
+    # Procesar y mostrar mensajes del asistente
+    for message in messages:
+        if message.run_id == run.id and message.role == "assistant":
+            full_response = process_message_with_citations(message)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+            with st.chat_message("assistant"):
+                st.markdown(full_response)
+                
+# Footer
+st.sidebar.markdown('---')
+st.sidebar.subheader('Creado por:')
+st.sidebar.markdown('Alexander Oviedo Fadul')
+st.sidebar.markdown("[GitHub](https://github.com/bladealex9848) | [Website](https://alexanderoviedofadul.dev/) | [LinkedIn](https://www.linkedin.com/in/alexander-oviedo-fadul/) | [Instagram](https://www.instagram.com/alexander.oviedo.fadul) | [Twitter](https://twitter.com/alexanderofadul) | [Facebook](https://www.facebook.com/alexanderof/) | [WhatsApp](https://api.whatsapp.com/send?phone=573015930519&text=Hola%20!Quiero%20conversar%20contigo!%20)")
